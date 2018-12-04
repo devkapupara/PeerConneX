b3f28a8b07dd4f5dee527e6215097bd6a34c4239
devkapupara <devkapupara@gmail.com>
2018-12-03 19:15:09 -0800

    Back functionality, show schedule

--- Results.java
+++ Results.java
@@ -3,27 +3,44 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableColumn;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.HashMap;
 
 public class Results extends JPanel
 {
-    Results(HashMap<String, Double> ratings, String headers[])
+    Results(HashMap<String, Double> ratings)
     {
         Color background = new Color(43,45,47);
         Color foreground = new Color(255,191,0);
         JLabel label = new JLabel("Results", SwingConstants.CENTER);
         label.setFont(label.getFont().deriveFont(35f));
         label.setForeground(foreground);
+        String[] headers = {"Name", "Rating"};
         JTable table = new JTable(ratings.size(), 2);
         table.setRowHeight(30);
         table.setForeground(foreground);
         table.setBackground(background);
+        table.setDefaultEditor(Object.class, null);
         JTableHeader jth = table.getTableHeader();
         jth.setFont(jth.getFont().deriveFont(20f));
         table.setFont(table.getFont().deriveFont(15f));
         jth.setBackground(background);
         jth.setForeground(foreground);
         table.setFillsViewportHeight(true);
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                JTable target = (JTable)e.getSource();
+                int row = target.getSelectedRow();
+                System.out.println(table.getValueAt(row,0));
+                JPanel data = new TutorSchedule((String)table.getValueAt(row,0), ratings);
+                removeAll();
+                add(data);
+                repaint();
+                revalidate();
+            }
+        });
         DefaultTableCellRenderer renderer = new DefaultTableCellRenderer();
         renderer.setHorizontalAlignment(SwingConstants.CENTER);
         for (int i = 0; i < table.getModel().getColumnCount(); i++)
@@ -53,8 +70,7 @@ public class Results extends JPanel
         add(east, "East");
         add(west, "West");
         add(south, "South");
-        table.setDefaultEditor(Object.class, null);
-        setSize(600,600);
+        setSize(500,500);
         setBackground(background);
         setVisible(true);
     }
---
+++ TutorSchedule.java
@@ -0,0 +1,100 @@
+import javax.swing.*;
+import javax.swing.border.MatteBorder;
+import java.awt.*;
+import java.sql.*;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
+
+public class TutorSchedule extends JPanel
+{
+    TutorSchedule(String name, HashMap<String, Double> oldData)
+    {
+        setLayout(new BorderLayout());
+        Color background = new Color(43,45,47);
+        Color foreground = new Color(255,191,0);
+        JLabel label = new JLabel(getData(name), SwingConstants.CENTER);
+        label.setFont(label.getFont().deriveFont(20f));
+        label.setForeground(foreground);
+        label.setBackground(background);
+        setBackground(background);
+        add(label, "Center");
+        JButton back = new JButton("  Back  ");
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setPreferredSize(new Dimension(100, 50));
+        buttonPanel.setBackground(background);
+        buttonPanel.add(back);
+        back.setForeground(foreground);
+        back.setFont(back.getFont().deriveFont(20f));
+        back.setBackground(background);
+        back.setBorder(new MatteBorder(1,1,1,1, foreground));
+        //back.setSize(300,50);
+        back.addActionListener(e -> {
+            removeAll();
+            add(new Results(oldData));
+            repaint();
+            revalidate();
+        });
+        add(back, "South");
+    }
+    private static String getData(String name) {
+
+        final String DB_URL = "jdbc:mysql://localhost:3306/project?useSSL=false";
+
+        final String USER = "dev";
+        final String PASS = "dev2511996";
+
+        Connection conn = null;
+        Statement stmt = null;
+        CallableStatement cs = null;
+        ResultSet rs = null;
+        StringBuilder sb = new StringBuilder("<html>" + name + "'s Schedule: <br>");
+        int out;
+        try{
+            //STEP 3: Open a connection
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            cs = conn.prepareCall("{CALL getID(?, ?)}");
+            cs.setString(1, name);
+            cs.registerOutParameter(2, Types.INTEGER);
+            boolean hasResult = cs.execute();
+            out = cs.getInt(2);
+
+            String query = String.format("Select day, start_time, end_time from tutor_schedule where id = % d", out);
+            //STEP 4: Execute a query
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+            SimpleDateFormat df = new SimpleDateFormat("hh:mm a");
+            //STEP 5: Process the results
+            while(rs.next()){
+                String day = rs.getString(1);
+                String startTime = df.format(rs.getTime(2));
+                String endTime = df.format(rs.getTime(3));
+                sb.append(String.format("%s from %s to %s.<br>", day, startTime, endTime));
+            }
+        } catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if (rs!=null)
+                    rs.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){}// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+
+        return sb.append("</html>").toString();
+    }
+}
